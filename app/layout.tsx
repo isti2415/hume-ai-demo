02754dc485c7c4215671fce987a143afd71d7996
Istiaq Ahmed <connect@istiaqahmed.me>
2024-05-01 22:55:18 +0600

    fixing opengraph and twiitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,25 @@ export const metadata: Metadata = {
   description:
     "An intelligent web application built with Next.js, TypeScript, Tailwind CSS, and the Hume AI React SDK, showcasing the power of AI in modern web development.",
   openGraph: {
-    images: "/opengraph-image.png"
+    title: "Hume AI Demo: Intelligent Web App with Next.js",
+    description:
+      "An intelligent web application built with Next.js, TypeScript, Tailwind CSS, and the Hume AI React SDK, showcasing the power of AI in modern web development.",
+    type: "website",
+    url: "https://humeaidemo.vercel.app",
+    images: [
+      {
+        url: "https://github.com/isti2415/hume-ai-demo/blob/c8df06715b6ca3274b033ee0088a666cc6e01d13/app/opengraph-image.png?raw=true",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
+    title: "Hume AI Demo: Intelligent Web App with Next.js",
+    description:
+      "An intelligent web application built with Next.js, TypeScript, Tailwind CSS, and the Hume AI React SDK, showcasing the power of AI in modern web development.",
+    images: [
+      "https://github.com/isti2415/hume-ai-demo/blob/c8df06715b6ca3274b033ee0088a666cc6e01d13/app/opengraph-image.png?raw=true",
+    ],
   },
 };
 
